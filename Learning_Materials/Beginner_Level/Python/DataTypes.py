77a00cac34dc83675e6b2919cbcfb426a76f3c03
Dreadfall <116603758+Dreadfall@users.noreply.github.com>
2023-07-10 09:23:17 -0500

    Created structure and some basic coursework

---
+++ Learning_Materials/Beginner_Level/Python/DataTypes.py
@@ -0,0 +1,36 @@
+# Hello! this is the data types file. Here we will learn about the data types in python.
+
+# Data types are the type of data that we can store in a variable. There are many data types in python. Some of them are:
+# 1. int (any whole number)
+a_number = 1
+# 2. float (any decimal number)
+a_decimal = -0.57382
+# 3. str (any string)
+a_string = "Hello World!"
+# 4. char (any single character)
+a_character = 'a'
+# 5. bool (A True or False)
+a_boolean = True
+the_other_boolean = False
+# and many more! However for now we will focus only on these
+
+# By now, you should notice that each of the data types follow a pattern. They
+# have a name, equals sign, then a value. This forms what I like to call the
+# "variable equation".
+#   variable_name = value
+
+# (something to note is that in python, you can just assign a variable a value
+# without declaring the data type. This will be different in java.)
+
+# Now, let's try printing out the values of these variables. To do this, we
+# will use the pre-made method print(). This method will print out whatever
+# is inside the parentheses. For example:
+
+# we can print a value
+print("Hello world! is 2+2 = 4?")
+# we can also print a variable
+print(a_boolean)
+
+# Now, if you want a bit of practice, try creating some of your own variables
+# and printing them out. You may also consider writing a reference sheet for
+# for those variables if you think you might forget them.
