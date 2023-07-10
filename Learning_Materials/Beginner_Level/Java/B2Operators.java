8bac12e148b39e57666d01697feb6c9353c5cc74
Dreadfall <116603758+Dreadfall@users.noreply.github.com>
2023-07-10 10:06:57 -0500

    Created operator file (needs work)

---
+++ Learning_Materials/Beginner_Level/Java/B2Operators.java
@@ -0,0 +1,10 @@
+public class B2Operators {
+  public static void main(String[] args) {
+    // This next section is going to go over what 'operators' are, and what they do!
+    // There are various operators, but the 4 main types are 
+      // - Arithmetic
+      // - Assignment
+      // - Comparison
+      // - Logical
+  }
+}
