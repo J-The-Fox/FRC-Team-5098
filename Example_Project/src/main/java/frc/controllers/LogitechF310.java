98613e8c35bd31f10c4019bd1bb208c472898e72
J-The-Fox <116409406+J-The-Fox@users.noreply.github.com>
2023-08-12 12:45:43 -0500

    Add Controllers and docs (Unfinished)

---
+++ Example_Project/src/main/java/frc/controllers/LogitechF310.java
@@ -0,0 +1,182 @@
+package frc.controllers;
+
+import edu.wpi.first.wpilibj.Joystick;
+import java.lang.Math;
+
+// Imported From Code/2022/stingr/main-bot
+// Note: This is modified from the original code to add deadbanding
+
+public class LogitechF310
+{
+    private final int kIdButtonA           = 1;
+    private final int kIdButtonB           = 2;
+    private final int kIdButtonX           = 3;
+    private final int kIdButtonY           = 4;
+    private final int kIdButtonBumperLeft  = 5;
+    private final int kIdButtonBumperRight = 6;
+    private final int kIdAxisleftX         = 0;
+    private final int kIdAxisleftY         = 1;
+    private final int kIdAxisrightX        = 4;
+    private final int kIdAxisrightY        = 5;
+    private final int kIdlefttrigger       = 2;
+    private final int kIdrighttrigger      = 3;
+    private final int kIdDpad              = 0;
+
+    // ...
+
+    private Joystick controller;
+
+    public double deadBand(double value)
+    {
+        return Math.abs(value) < 0.2 ? 0 : value;
+    }
+
+    public LogitechF310(int id)
+    {
+        controller = new Joystick(id);
+    }
+
+    public boolean getA()
+    {
+        return controller.getRawButton(kIdButtonA);
+    }
+
+    public boolean getB()
+    {
+        return controller.getRawButton(kIdButtonB);
+    }
+
+    public boolean getX()
+    {
+        return controller.getRawButton(kIdButtonX);
+    }
+
+    public boolean getY()
+    {
+        return controller.getRawButton(kIdButtonY);
+    }
+
+    public boolean getLeftBumper()
+    {
+        return controller.getRawButton(kIdButtonBumperLeft);
+    }
+
+    public boolean getRightBumper()
+    {
+        return controller.getRawButton(kIdButtonBumperRight);
+    }
+
+    public double getLeftStickX()
+    {
+        return deadBand(controller.getRawAxis(kIdAxisleftX));
+    }
+
+    public double getLeftStickY()
+    {
+        return deadBand(controller.getRawAxis(kIdAxisleftY));
+    }
+
+    public double getRightStickX()
+    {
+        return deadBand(controller.getRawAxis(kIdAxisrightX));
+    }
+
+    public double getRightStickY()
+    {
+        return deadBand(controller.getRawAxis(kIdAxisrightY));
+    }
+
+    public double getLeftTrigger()
+    {
+        return controller.getRawAxis(kIdlefttrigger);
+    }
+
+    public double getRightTrigger()
+    {
+        return controller.getRawAxis(kIdrighttrigger);
+    }
+
+    public boolean getDUp()
+    {
+        int degrees = controller.getPOV(kIdDpad);
+        if (degrees == 315)
+        {
+            return true;
+        }
+
+        if (degrees == 0)
+        {
+            return true;
+        }
+
+        if (degrees == 45)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean getDRight()
+    {
+        int degrees = controller.getPOV(kIdDpad);
+        if (degrees == 45)
+        {
+            return true;
+        }
+
+        if (degrees == 90)
+        {
+            return true;
+        }
+
+        if (degrees == 135)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean getDDown()
+    {
+        int degrees = controller.getPOV(kIdDpad);
+        if (degrees == 135)
+        {
+            return true;
+        }
+
+        if (degrees == 180)
+        {
+            return true;
+        }
+
+        if (degrees == 225)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean getDLeft()
+    {
+        int degrees = controller.getPOV(kIdDpad);
+        if (degrees == 225)
+        {
+            return true;
+        }
+
+        if (degrees == 270)
+        {
+            return true;
+        }
+
+        if (degrees == 315)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Example_Project/src/main/java/frc/controllers/README.md
@@ -0,0 +1,5 @@
+# Controllers
+
+This folder is used to store the controller files that are used to intereact with the controllers we use to control the robot  
+Altough this folder is not required, it is useful for organization
+
