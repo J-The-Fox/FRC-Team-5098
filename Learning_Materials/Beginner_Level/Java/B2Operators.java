ec7d8658b67e683e94b29e9372bdfe3be4469f5a
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2023-08-15 22:25:12 -0500

    added Operators into the course

--- Learning_Materials/Beginner_Level/Java/B2Operators.java
+++ Learning_Materials/Beginner_Level/Java/B2Operators.java
@@ -1,10 +1,33 @@
 public class B2Operators {
   public static void main(String[] args) {
     // This next section is going to go over what 'operators' are, and what they do!
-    // There are various operators, but the 4 main types are 
-      // - Arithmetic
-      // - Assignment
-      // - Comparison
-      // - Logical
+    // There are various operators, but the 4 main types are
+    // - Arithmetic: adding, subtracting, multiplying, dividing, etc.
+    // - Assignment: assigning a value to a variable
+    // - Comparison: comparing two values
+    // - Logical: if this AND this, then that, (hope that made sense, basically it's
+    // just "AND" or "OR")
+
+    // Examples
+
+    // Arithmetic
+    int x = 5 + 10; // the "+" is an example of an arithmatic operator
+
+    // Assignment
+    int y = 5; // the "=" is an example of an assignment operator
+
+    // the next part contains if statements, which we will go over in the next
+    // section
+    // so for now just focus on the operators
+
+    // Comparison
+    if (x == y) { // the "==" is an example of a comparison operator
+      System.out.println("x is equal to y!");
+    }
+
+    // Logical
+    if (x == 5 && y == 5) { // the "&&" is an example of a logical operator
+      System.out.println("x and y are both equal to 5!");
+    }
   }
 }
---
+++ Learning_Materials/Beginner_Level/Python/B2Operators.py
@@ -0,0 +1,26 @@
+#  This next section is going to go over what 'operators' are, and what they do!
+#      There are various operators, but the 4 main types are
+#      - Arithmetic: adding, subtracting, multiplying, dividing, etc.
+#      - Assignment: assigning a value to a variable
+#      - Comparison: comparing two values
+#      - Logical: if this AND this, then that, (hope that made sense, basically it's
+#        just "AND" or "OR")
+
+# Examples
+
+# Arithmetic Operators
+x = 5 + 4 # The "+" is an arithmetic operator
+
+# Assignment Operators
+x = 5 # The "=" is an assignment operator
+
+# the next part contains "if statements", which are used to check if a condition is true\
+# We will cover these in more detail later, but for now just focus on the operators
+
+# Comparison Operators
+if x == 5: # The "==" is a comparison operator
+    print("x is equal to 5!")
+
+# Logical Operators
+if x == 5 and x > 4: # The "and" is a logical operator
+    print("x is equal to 5 and greater than 4!")
