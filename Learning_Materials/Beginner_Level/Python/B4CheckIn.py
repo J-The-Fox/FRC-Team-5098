256a458e61dcad263f82595e59f5a4b124a24966
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2023-08-16 22:40:54 -0500

    Added Checkin's for the course

---
+++ Learning_Materials/Beginner_Level/Python/B4CheckIn.py
@@ -0,0 +1,9 @@
+# Hello! this is just a quick check in to see how you are doing with the course so far.
+
+# if you have any feedback, please let someone know! This whole thing is still very new and we are working out the kinks as we go along.
+
+# anyway, here are some little exercises you can do to make sure you are understanding the material so far
+
+# Question 1: How are Comparison and Logical Operators used in If statements?
+
+# Question 2: How many "else-if"s can you have in single if statement
