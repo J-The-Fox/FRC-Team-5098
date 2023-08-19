15a9f12af08e9a75aeabf37a9ea9f47b364c9562
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2023-08-19 18:34:47 -0500

    Created a final check for the beginner level (B8)

---
+++ Learning_Materials/Beginner_Level/Java/B8FinalCheckIn.java
@@ -0,0 +1,22 @@
+public class B8FinalCheckIn {
+  // Hello! Congrats, your done learning with the beginner level!
+
+  // Here are some exercises you can do to make sure that you understand the
+  // material so far
+
+  // What is the difference between a double and an integer? (B1)
+
+  // What does the modulus operator do? How might it be useful? (B2)
+
+  // What operator adds a value to itself? (B2)
+
+  // How are functions useful? (B5)
+
+  // What are parameters? (B5)
+
+  // What is the difference between a class and a object? (B7)
+
+  // Why do we use classes? (B7)
+
+  // why do we sometimes use a constructor when creating classes? (B7)
+}
---
+++ Learning_Materials/Beginner_Level/Python/B8FinalCheckIn.py
@@ -0,0 +1,19 @@
+# Hello! Congrats, your done learning with the beginner level!
+
+# Here are some exercises you can do to make sure that you understand the material so far
+
+# what is the difference between a float and an integer? (B1)
+
+# what does the modulus operator do? How might it be useful? (B2)
+
+# what operator adds a value to itself? (B2)
+
+# How are functions useful? (B5)
+
+# What are parameters? (B5)
+
+# What is the difference between a class and a object? (B7)
+
+# Why do we use classes? (B7)
+
+# why do we use the __init__ function when creating classes? (B7)
