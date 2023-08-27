9d7bd36d403be4b54a74f4dd079a906f5764f31f
Henry Hare <dreadfall64@gmail.com>
2023-08-27 12:21:24 -0500

    Created CheckIn for intermediate course (I4)

---
+++ Learning_Materials/Intermediate_Level/Java/I4CheckIn.java
@@ -0,0 +1,18 @@
+public class I4CheckIn {
+  public static void main(String[] args) {
+    // Hey! congrats on making it to the check in, hope the previous stuff wasn't too hard :)
+    // A small note, if you have any questions, or aren't completely sure how something works, now
+    // is a good time to ask! As we will be building on these concepts, and it is no fun to just
+    // get stuck super far ahead and have no idea what is going on.
+
+    // anyways, here are some questions
+
+    // 1. How do you convert a string to an integer?
+    // 2. How can you use a loop in a for loop?
+    // 3. How can a while loop (or for loop) get stuck in an infinite loop?
+
+    // A fun exercise you can do
+      // Create a program that takes a word from the user, and it's definition, and stores it in a dictionary array
+      // Include an array, loop, and function
+  }
+}
