8a365b0fb3b7a2aa83ae060049f9d8efd70f4251
Jay <j.t.mcneely06@gmail.com>
2023-09-01 20:56:50 -0500

    Updated formatting and added controller example link
    Just updated the some formatting with multi-line comments and added the controller example in the README.md in the example project (Added a link)

--- Learning_Materials/Intermediate_Level/Java/I5Enums.java
+++ Learning_Materials/Intermediate_Level/Java/I5Enums.java
@@ -1,7 +1,9 @@
 public class I5Enums {
   public static void main(String[] args) {
-    // Enums aren't often as used as the other variables, but we still use them in robotics, so it's 
-    // important to know what they are. They are basically a way to store a set of constants (variables that cannot be changed)
+    /**
+     * Enums aren't often as used as the other variables, but we still use them in robotics, so it's important to know what they are. 
+     * They are basically a way to store a set of constants (variables that cannot be changed)
+     */
 
     // Here is an example of an enum
     enum Direction {
@@ -10,9 +12,15 @@ public class I5Enums {
       EAST,
       WEST
     }
+    
     // We know that the direction of a robot can only be one of the four directions, so we can use an enum to store the directions
     Direction robotDirection = Direction.NORTH; // robotDirection is now NORTH
     robotDirection = Direction.SOUTH; // robotDirection is now SOUTH
     // this way, we can't accidentally set robotDirection to something that isn't a direction
+
+    /**
+     * A good example would be the code that is used for the controller dpad.
+     * Ex: https://github.com/J-The-Fox/FRC-Team-5098/blob/main/Example_Project/src/main/java/frc/controllers/README.md
+     */
   }
 }
