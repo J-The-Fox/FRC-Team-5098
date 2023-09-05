2a9eae9777e60ed7fe07c6520cdaac40eecb0149
Jay <j.t.mcneely06@gmail.com>
2023-09-05 16:27:18 -0500

    Created new components

---
+++ Example_Project/src/main/java/frc/components/Pneumatics.java
@@ -0,0 +1,55 @@
+package frc.components;
+
+// Local Imports
+import frc.CommonData;
+import frc.settings.PneumaticsSettings;
+
+// WPILib Imports
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+public class Pneumatics implements IComponent{
+    
+    // Create an enum for the states of the solenoids
+    public enum State {
+        kOpen,
+        kClosed,
+    }
+
+    // Create a new solenoid object
+    private DoubleSolenoid m_solenoidLarge;
+    private DoubleSolenoid m_solenoidSmall;
+
+    public Pneumatics(PneumaticsSettings settings) {
+        // Set the settings for the solenoids using the settings in PneumaticsSettings.java
+        m_solenoidLarge = new DoubleSolenoid(settings.pneumaticsModuleID, PneumaticsModuleType.REVPH, settings.largeSolenoidForwardChannelID, settings.largeSolenoidReverseChannelID);
+        m_solenoidSmall = new DoubleSolenoid(settings.pneumaticsModuleID, PneumaticsModuleType.REVPH, settings.smallSolenoidForwardChannelID, settings.smallSolenoidReverseChannelID);
+    };
+
+    public void update(){
+
+        // Note: This assumes that the CommonData values are updated elsewhere
+
+        // Update the solenoid state for the small solenoid
+        switch (CommonData.smallSolenoidState)
+        {
+        case kOpen:
+            m_solenoidSmall.set(Value.kForward);
+            break;
+        case kClosed:
+            m_solenoidSmall.set(Value.kReverse);
+        }
+
+        // Update the solenoid state for the large solenoid
+        switch (CommonData.largeSolenoidState)
+        {
+        case kOpen:
+            m_solenoidLarge.set(Value.kForward);
+            break;
+        case kClosed:
+            m_solenoidLarge.set(Value.kReverse);
+        }
+    }
+}
+
---
+++ Example_Project/src/main/java/frc/components/ShuffleBoardController.java
@@ -0,0 +1,20 @@
+package frc.components;
+
+// Local Imports
+import frc.settings.ShuffleBoardSettings;
+import frc.CommonData;
+
+public class ShuffleBoardController implements IComponent{
+    // This is the ShuffleBoard Class. It contains methods and functions that are used to display data on the ShuffleBoard.
+
+
+    public ShuffleBoardController(ShuffleBoardSettings settings) {
+        // This is the ShuffleBoardSettings object. It contains all of the settings for the ShuffleBoard.
+        // It is initialized in the Settings.java file.
+    };
+
+    public void update(){
+        // Place any Shuffleboard values here
+    }
+
+}
