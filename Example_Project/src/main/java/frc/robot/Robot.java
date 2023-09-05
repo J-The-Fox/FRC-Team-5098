3bd98ad023049ef01e1e111fba3bf792a2f616c7
Jay <j.t.mcneely06@gmail.com>
2023-09-05 16:28:00 -0500

    Set up formating for other files

--- Example_Project/src/main/java/frc/robot/Robot.java
+++ Example_Project/src/main/java/frc/robot/Robot.java
@@ -4,7 +4,23 @@
 
 package frc.robot;
 
+// Internal Imports
+import java.io.IOException;
+
+// External Imports
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// Local Imports
+import frc.CommonData;
+import frc.components.IComponent;
+import frc.components.ShuffleBoardController;
+import frc.settings.Settings;
+
+// WPILib / Vendordep Imports
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Compressor;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -12,11 +28,37 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
+
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+
+  // Read the robot_settings.json file and put it into the settings object.
+  public static Settings settings;
+  static {
+      ObjectMapper mapper = new ObjectMapper();
+      // Try to read the robot_settings.json file and put it into the settings object.
+      try {
+          var filepath = Filesystem.getDeployDirectory().toPath().resolve("robot_settings.json");
+          settings     = mapper.readValue(filepath.toFile(), Settings.class);
+      }
+      // If there is an error, print the error to the console.
+      catch (IOException ex) {
+          System.out.println(ex.toString());
+      }
+  }
+
+  // Create a new compressor object to control the pneumatics.
+
+  // This could possible be moved to the Pneumatics.java component. 
+  // I would like to see if it's possbible to see what types of a pnematic module is currently plugged in.
+  // However I am not sure if that is possible. I may look into it in the future.
+  Compressor compressor = new Compressor(settings.pneumaticsSettings.pneumaticsModuleID, PneumaticsModuleType.REVPH);
+
+  // Create a new IComponent array and add all of the components to it.
+  // This is used to call all the update methods in the periodic() method.
+  IComponent[] RobotComponents = new IComponent[] {
+    new ShuffleBoardController(settings.shuffleBoardSettings),
+
+  };
 
   @Override
   public void robotInit() {
@@ -26,6 +68,11 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     // This is ran every 20ms while the robot is on. This is where you should add any code that needs to run constantly.
+
+    // This calls all the update methods in the RobotComponents array to update all components.
+    for (IComponent component : RobotComponents) {
+      component.update();
+    }
   }
 
   @Override
@@ -37,6 +84,10 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     // This is ran every 20ms while the robot is in autonomous mode.
+
+    for (IComponent component : RobotComponents) {
+      component.update();
+    }
   }
 
   @Override
