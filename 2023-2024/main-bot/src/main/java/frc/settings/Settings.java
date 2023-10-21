ada9a61335808cc1f335c3e01683525a6d5293fc
J-The-Fox <116409406+J-The-Fox@users.noreply.github.com>
2023-10-20 20:42:06 -0500

    Add settings files

---
+++ 2023-2024/main-bot/src/main/java/frc/settings/Settings.java
@@ -0,0 +1,10 @@
+package frc.settings;
+
+public class Settings {
+
+    public SwerveDriveSettings swerveDrive;
+
+    // These might change to be more dynamic if possible
+    public int controllerID;
+    public int auxControllerID;
+}
---
+++ 2023-2024/main-bot/src/main/java/frc/settings/SwerveDriveSettings.java
@@ -0,0 +1,9 @@
+package frc.settings;
+
+public class SwerveDriveSettings {
+
+    public SwerveWheelSettings FLSwerve;
+    public SwerveWheelSettings FRSwerve;
+    public SwerveWheelSettings BLSwerve;
+    public SwerveWheelSettings BRSwerve;
+}
