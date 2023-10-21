6cd536e60315f47efbd2a7418c54ff8fd91b107b
J-The-Fox <116409406+J-The-Fox@users.noreply.github.com>
2023-10-20 20:42:26 -0500

    Create CommonData.java

---
+++ 2023-2024/main-bot/src/main/java/frc/CommonData.java
@@ -0,0 +1,16 @@
+package frc;
+
+public class CommonData {
+
+    // Swerve Drive Variables
+    public static double desiredTurn;
+    public static double forwardSpeed;
+    public static double sideSpeed;
+    public static boolean battenDownTheHatches;
+
+    // NavX Variables
+    public static boolean calibrate;
+
+    // Autonomous Varaibles
+    public static int counter;
+}
