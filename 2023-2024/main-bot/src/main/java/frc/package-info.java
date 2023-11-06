c58fb2a4c773df6ff2fa6cbf744ff2a0f7b6ead1
J-The-Fox <116409406+J-The-Fox@users.noreply.github.com>
2023-11-06 06:02:05 -0600

    Add test package-info.java file

---
+++ 2023-2024/main-bot/src/main/java/frc/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * Main package for the robot code.
+ * <p>
+ * This is a test currently being used to test the javadoc generation
+ * and fix code linting issues.
+ * </p>
+ */
+package frc;
