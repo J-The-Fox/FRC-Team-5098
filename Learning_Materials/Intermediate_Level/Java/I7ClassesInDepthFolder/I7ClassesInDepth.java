75cd24ba103eb148836788c5e36ddad823c4995f
Henry Hare <dreadfall64@gmail.com>
2024-01-07 13:42:36 -0600

    I7 Lesson Structure

---
+++ Learning_Materials/Intermediate_Level/Java/I7ClassesInDepthFolder/I7ClassesInDepth.java
@@ -0,0 +1,8 @@
+public class I7ClassesInDepth {
+  public static void main(String[] args){
+    // Packages
+    // Keywords (public and static) 
+    // Inheritance 
+    // Maybeeee touch on interface?
+  }
+}
