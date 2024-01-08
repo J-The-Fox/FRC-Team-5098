8ab00acb46c313f04a8db223b9083c204f6d5c67
Jay <j.t.mcneely06@gmail.com>
2024-01-08 09:09:59 -0600

    Update documention for swerve drive code

--- 2023-2024/main-bot/src/main/java/frc/components/SwerveDrive.java
+++ 2023-2024/main-bot/src/main/java/frc/components/SwerveDrive.java
@@ -10,13 +10,80 @@ import frc.settings.SwerveDriveSettings;
 
 public class SwerveDrive implements IComponent {
 
-    // This needs to be recaulculated for the new swerve drive and chassis
-    // TODO: Add documentation for this so each value is explained
-    private final static double MAX_SPEED    = (6380 * 0.3191858136) / 60 / 3;
+    /**
+     * Overall Equation:
+     *
+     * Motor roations per second:
+     * Motor rotations per minute / 60
+     *
+     * Wheel rotations per second:
+     * Motor rotations per second / gear ratio
+     *
+     * Wheel radius (in ft):
+     * Wheel diameter (in ft) / 2
+     * or
+     * Wheel radius (in inches) / 12
+     *
+     * Wheel circumference (in ft):
+     * Wheel radius (in ft) * 2 * pi
+     *
+     * Speed (in ft/s):
+     * Wheel rotations per second * wheel circumference (in ft)
+     *
+     * Total Equation:
+     * (Motor rotations per minute / 60) / gear ratio * wheel circumference
+     * (in ft)
+     *
+     * Note: The way it is lined out in the code is different but it still
+     * functions the same.
+     *
+     * Note: Phoenix 6 has a Swerve Drive API that can be utilized.
+     *       It may be worth looking into.
+     *
+     * Note: These values are *not* correct for our current frame.
+     *       These are for the 2022-2023 robot, they need to be updated.
+     */
+
+    /**
+     * The Falcon 500's RPM.
+     */
+    private final static int FALCON_RPM = 6380;
+
+    /**
+     * Gear ratio of the swerve modules.
+     */
+    private final static double GEAR_RATIO = 3;
+
+    /**
+     * Wheel circumference in feet.
+     */
+    private final static double WHEEL_CIRCUMFERENCE = 0.3191858136;
+
+    /**
+     * The maximum speed of the robot.
+     *
+     * <p>
+     * 60 is used to convert from minutes to seconds.
+     * </p>
+     */
+    private final static double MAX_SPEED    = (FALCON_RPM * WHEEL_CIRCUMFERENCE) / 60 / GEAR_RATIO;
+
+    /**
+     * The maximum rotation of the robot.
+     */
     private final static double MAX_ROTATION = (MAX_SPEED * 2 * Math.PI) / 2.7389195456 / 2;
 
+    // The 2.7389195456 value still needs to be documented
+
+    /**
+     * The offset of the yaw.
+     */
     double yawOffset = 0;
 
+    /**
+     * The constructor for the SwerveDrive class.
+     * @param settings  The settings for the swerve drive.
+     */
     public SwerveDrive(final SwerveDriveSettings settings) {
         swerveWheels = new SwerveWheel[] {
             new SwerveWheel(settings.getFLSwerveSettings()),
